759d172337fa78826ebc73291a3eb55561690271
Beth-Anne Farmer <bethannef@protonmail.com>
2022-03-01 20:23:56 -0500

    multer start

--- routes/clubs.js
+++ routes/clubs.js
@@ -4,11 +4,19 @@ const clubs = require('../controllers/clubs');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateClub} = require('../middleware');
 const Club = require('../models/club');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'})
+
+
 
 
 router.route('/')
     .get(catchAsync(clubs.index))
-    .post(isLoggedIn, validateClub, catchAsync(clubs.createClub))
+    //.post(isLoggedIn, validateClub, catchAsync(clubs.createClub))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('it worked...');
+    })
 
 router.get('/new', isLoggedIn, clubs.renderNewForm);
 
